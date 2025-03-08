4c084810c7070b548415163934ee733188ab68c6
Rajat083 <155555359+Rajat083@users.noreply.github.com>
2025-03-09 02:59:05 +0530

    Added neural network

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from nltk.stem import PorterStemmer as ps
 import string
 import pickle 
 
+nltk.download('stopwords')
+nltk.download('punkt')
+
 def text_preprocess(text):
     text = text.lower()
     text = nltk.word_tokenize(text)
@@ -26,10 +29,10 @@ app = Flask(__name__)
 
 model_bnb = pickle.load(open('spam_model.pkl', 'rb'))
 model_rfc = pickle.load(open('spam.pkl', 'rb'))
-# model_svc = pickle.load(open('spam_svc.pkl', 'rb'))
-# text_preprocess = pickle.load(open('spam_preprocess.pkl', 'rb'))
+model_ann = pickle.load(open('ann.pkl', 'rb'))
+
 tfidf = pickle.load(open('spam_vectorizer.pkl', 'rb'))
-predictions_meanig = {1 : 'Spam', 0 : 'Not Spam'}
+predictions_meaning = {1 : 'Spam', 0 : 'Not Spam'}
 
 def colorAdder(str, result):
     if result == 'Spam':
@@ -48,26 +51,19 @@ def test():
     vectorized_text = tfidf.transform([preprocessed_text])
 
     predictions = [model_bnb.predict(vectorized_text), model_rfc.predict(vectorized_text)]
-    model_pred = [predictions_meanig[predictions[0][0]],predictions_meanig[predictions[1][0]]]
+    model_pred = [predictions_meaning[predictions[0][0]],predictions_meaning[predictions[1][0]], predictions_meaning[int(model_ann.predict(vectorized_text)[0][0] > 0.5)]]
     bnb_text = f'Naive Bayes Classifier Prediction: {model_pred[0]}\n'
-    rfc_text = f'Random Forest Classifier Prediction: {model_pred[1]}'
+    rfc_text = f'Random Forest Classifier Prediction: {model_pred[1]}\n'
+    ann_text = f'Artificial Neural Network Prediction: {model_pred[2]}\n'
     
-    return render_template('index.html', bnb=bnb_text, rfc=rfc_text, bnb_result=predictions[0][0], rfc_result=predictions[1][0])
-
-
-# st.title('E-mail Spam Classifier')
-# input = st.text_input('Enter the e-mail text')
-
-# if st.button('Predict'):
-#     preprocessed_text = text_preprocess(input)
-
-#     vectorized_text = tfidf.transform([preprocessed_text])
-
-#     predictions_meanig = {1 : 'Spam', 0 : 'Not Spam'}
-#     predictions = [model_bnb.predict(vectorized_text), model_svc.predict(vectorized_text)]
-
-#     st.write(f'Naive Bayes Classifier Prediction: {predictions_meanig[predictions[0][0]]}')
-#     st.write(f'Support Vector Classifier Prediction: {predictions_meanig[predictions[1][0]]}')
+    return render_template('index.html', 
+                        bnb=bnb_text or "No prediction", 
+                        rfc=rfc_text or "No prediction", 
+                        ann=ann_text or "No prediction",
+                        bnb_result=predictions[0][0] if predictions else 0, 
+                        rfc_result=predictions[1][0] if predictions else 0,
+                        ann_result=model_ann.predict(vectorized_text)[0] if predictions else 0,
+                        )
 
 if __name__ == '__main__':
     app.run(debug=True)
